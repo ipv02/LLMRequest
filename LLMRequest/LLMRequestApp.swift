72b5add1785240cc04e09f4173f18b7de70f6764
Igor P-V <ispogiba@gmail.com>
2026-06-01 16:01:21 +0300

    Initial Commit

---
+++ LLMRequest/LLMRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLMRequestApp.swift
+//  LLMRequest
+//
+//  Created by Igor Pogiba-Vishnevskiy on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct LLMRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
